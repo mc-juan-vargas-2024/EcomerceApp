3acea1a52aa784bf07e54f6193fb2677451608fc
juan vargas <jvargas@unab.edu.co>
2025-04-03 19:41:53 -0500

    Diseño LoginScreen

---
+++ app/src/main/java/co/edu/unab/jorgebalaguera/ecomerceapp/LoginScreen.kt
@@ -0,0 +1,121 @@
+package co.edu.unab.jorgebalaguera.ecomerceapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import co.edu.unab.jorgebalaguera.ecomerceapp.ui.theme.EcomerceAppTheme
+
+@Composable
+fun LoginScreen() {
+    Scaffold { valuesPadding ->
+        Column(
+            modifier = Modifier
+                .padding(valuesPadding)
+                .fillMaxSize()
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource((R.drawable.logo)),
+                contentDescription = "logo unab",
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "iniciar sesion",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFFF9900)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("correo Electronico")
+                },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "email",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("contraseña")
+                },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "contraseña",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF9900)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "iniciar sesion",
+                    fontSize = 16.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+            TextButton(onClick = {}) {
+                Text(text = "¿no tienes una cuenta? Registrate", color = Color(0xFFFF9900))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    EcomerceAppTheme {
+        LoginScreen()
+    }
+}
