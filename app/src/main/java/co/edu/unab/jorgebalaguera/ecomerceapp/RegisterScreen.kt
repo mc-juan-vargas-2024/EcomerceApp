d7dc5a19af562c1d84a1d4ce33b97b70dcdae839
juan vargas <jvargas@unab.edu.co>
2025-04-08 18:46:27 -0500

    diseño del RegisterScreen

---
+++ app/src/main/java/co/edu/unab/jorgebalaguera/ecomerceapp/RegisterScreen.kt
@@ -0,0 +1,153 @@
+package co.edu.unab.jorgebalaguera.ecomerceapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import co.edu.unab.jorgebalaguera.ecomerceapp.ui.theme.EcomerceAppTheme
+
+@Composable
+fun RegisterScreen() {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Register",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Text(
+                text = "Registrar Usuario",
+                fontSize = 24.sp,
+                color = Color(0xFFFF9900),
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("nombre completo")
+                },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Face,
+                        contentDescription = "email",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("correo electronico")
+                },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "contraseña",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("contraseña")
+                },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "email",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text("confirmar contraseña")
+                },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "contraseña",
+                        tint = Color(0xFFFF9900)
+                    )
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF9900)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "registrarse",
+                    fontSize = 16.sp
+                )
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RegisterScreenPreview() {
+    EcomerceAppTheme {
+        RegisterScreen()
+    }
+
+}
